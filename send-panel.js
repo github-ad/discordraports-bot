694bd4842988b52dbd1b07e512ca18643fdce3c6
github-ad <76952424+github-ad@users.noreply.github.com>
2026-01-04 22:15:42 +0700

    Create send-panel.js

---
+++ send-panel.js
@@ -0,0 +1,35 @@
+const {
+  Client,
+  GatewayIntentBits,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
+} = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", async () => {
+  const channel = await client.channels.fetch(
+    process.env.PANEL_CHANNEL_ID
+  );
+
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId("open_report_modal")
+      .setLabel("📄 ZŁÓŻ RAPORT")
+      .setStyle(ButtonStyle.Primary)
+  );
+
+  await channel.send({
+    content: "**Los Santos Police Department – Axon Records**\nKliknij przycisk poniżej, aby złożyć raport z interwencji.",
+    components: [row]
+  });
+
+  console.log("Panel wysłany");
+  process.exit();
+});
+
+client.login(process.env.TOKEN);
