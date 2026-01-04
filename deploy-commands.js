c94000ff90de574e91bb59945cdaf99943e5f1ee
github-ad <76952424+github-ad@users.noreply.github.com>
2026-01-04 22:14:35 +0700

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,18 @@
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("panel")
+    .setDescription("Wyślij panel składania raportów (LSPD)")
+].map(c => c.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+rest.put(
+  Routes.applicationGuildCommands(
+    process.env.CLIENT_ID,
+    process.env.GUILD_ID
+  ),
+  { body: commands }
+);
